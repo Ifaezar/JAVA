aeff8490a85a75e4264d9760b6303418e0da9eb7
Ifaezar <faezarilham@gmail.com>
2020-05-29 15:45:39 +0700

    Weekend Task

--- CicilanCalculator/src/Show.java
+++ CicilanCalculator/src/Show.java
@@ -5,26 +5,31 @@ import java.text.NumberFormat;
 public class Show {
     private static final int BUlAN = 12;
     private static final int PERCENT = 100;
+    private static double totalBunga;
+    private static double totalBulan;
+    private static double sisaBayaran;
 
-	public static void sisaBayaranPerBulan(int hargaAwal, double bunga, int durasi, double bayaranPerBulan) {
-	    double sisaBayaran = hargaAwal + (hargaAwal * (bunga / PERCENT));
-	    System.out.println("================ Sisa Bayaran Tiap Bulan ================");
-	    for (int i = 0; i < durasi * BUlAN; i++) {
-            sisaBayaran -= bayaranPerBulan;
-            bayar(sisaBayaran);
-	    }
-	}
+    public static double bayaranTiapBulan(int hargaAwal, double bunga, int durasi) {
+        totalBunga = bunga / PERCENT;
+        totalBulan = durasi * BUlAN;
+        sisaBayaran = hargaAwal + (hargaAwal * totalBunga);
+        return sisaBayaran / totalBulan;
+    }
 
-	public static void tagihanPerBulan(double bayaranPerBulan) {
+    public static void tagihanPerBulan(double bayaranPerBulan) {
         System.out.println("================ Pembayaran Tiap Bulan ================");
         bayar(bayaranPerBulan);
-	}
+    }
 
-	public static double bayaranTiapBulan(int hargaAwal, double bunga, int durasi) {
-	    return (hargaAwal + (hargaAwal * (bunga / PERCENT))) / (durasi * BUlAN);
-	}
-    
-    public static void bayar(double totalHarga ){
+    public static void sisaBayaranPerBulan(int hargaAwal, double bunga, int durasi, double bayaranPerBulan) {
+        System.out.println("================ Sisa Bayaran Tiap Bulan ================");
+        for (int i = 0; i < totalBulan; i++) {
+            sisaBayaran -= bayaranPerBulan;
+            bayar(sisaBayaran);
+        }
+    }
+
+    public static void bayar(double totalHarga) {
         System.out.println(NumberFormat.getCurrencyInstance().format(totalHarga));
     }
 }
